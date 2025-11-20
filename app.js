c22b67f53359b1353baaeedabc48ca8341d669a3
Kaelen Cook <kaelen1cook@gmail.com>
2025-11-19 16:27:47 -0800

    better icons

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ function Header() {
     return <div className={`main ${state || ''}`}>
         <Title title={mainTitle} id="main" />
         <Title title={state ? state.charAt(0).toUpperCase() + state.slice(1) : subTitle} id="sub"/>
-        {state !== null && <SexyButton id="back" onClick={back} page={'BackIcon'}/>}
+        {state !== null && <BackButton id="back" onClick={back} page={'BackIcon'}/>}
         {/* {state === "about" && <Title title="About Me" />} */}
         {state === "projects" && <Projects section={section} onSelect={selectSection}/>}
         {/* {state === "contact" && <Title title="Contact Me" />} */}
@@ -80,9 +80,12 @@ function Header() {
         {/* {state === "blog" && <Title title="Blog" />} */}
         <div id="buttonContainer">
         <div id="buttons">
-            {pages.map((page) => (
-                <Button key={page} page={page} className="nav" onClick={() => handleClick(page)} image={"assets/icons/"+page+".svg"} />
-            ))}
+            <AboutButton id="about" onClick={() => handleClick("About")} page={'About'}/>
+            <ProjectsButton id="projects" onClick={() => handleClick("Projects")} page={'Projects'}/>
+            <ContactButton id="contact" onClick={() => handleClick("Contact")} page={'Contact'}/>
+            {/* {pages.map((page) => (
+                <Button key={page} page={page} id={page.toLowerCase()} className="nav" onClick={() => handleClick(page)} image={"assets/icons/"+page+".svg"} />
+            ))} */}
         </div>
         </div>
     </div>
@@ -101,8 +104,8 @@ function Page() {
     }
 
     return <div>
-        <BG />
         <Header />
+        <BG />
         {/* <button onClick={handleClick}>Click {value}</button> */}
     </div>;
 }
@@ -152,7 +155,7 @@ function Button({image, className, onClick, page, id}) {
     </button>;
 }
 
-function SexyButton({image, className, onClick, page, id}) {
+function BackButton({image, className, onClick, page, id}) {
     return <button className={className ? className : "button"} id={id} onClick={onClick}>
         <svg 
             className="icon icon-backicon"
@@ -181,6 +184,201 @@ function SexyButton({image, className, onClick, page, id}) {
     </button>;
 }
 
+function AboutButton({image, className, onClick, page, id}) {
+    return <button className={className ? className : "button"} id={id} onClick={onClick}>
+        <svg 
+            className="icon icon-about"
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 300 300" 
+            width="50" 
+            height="50"
+        >
+            <path 
+                id="e5EjUZ76vzl2" 
+                d="M100,236.70083v-24.088874c0-15.836314,0-83.241914-50-83.241914s-50,67.4056-50,83.241914Q0,236.70083,0,236.70083t100,0" 
+                transform="translate(100 0)" 
+                fill="none" 
+                stroke="currentColor" 
+                strokeWidth="4"
+            />
+            <g 
+                id="e5EjUZ76vzl3_to" 
+                style={{
+                    offsetPath: "path('M150,95.640606Q142.134284,92.611404,135.781771,97.220409Q135.781771,97.220409,135.781771,97.220409C140.239477,93.146955,146.737799,93.817647,150,95.640606')",
+                    offsetRotate: "0deg"
+                }}
+            >
+                <ellipse 
+                    rx="29.418776" 
+                    ry="27.096241" 
+                    transform="translate(0,0)" 
+                    fill="none" 
+                    stroke="currentColor" 
+                    strokeWidth="4"
+                />
+            </g>
+            <g 
+                transform="matrix(1.123989 0 0 1.123989 6.549419 6.16794)" 
+                mask="url(#e5EjUZ76vzl6)"
+            >
+                <g 
+                    id="e5EjUZ76vzl5_to" 
+                    style={{
+                        offsetPath: "path('M158.876765,176.046602C255.252646,178.536946,214.675031,119.288851,193.039173,90.008197C215.626804,96.438676,221.702547,107.099665,221.702551,119.288854Q221.837762,97.92846,193.039173,90.008197C229.830311,178.736287,195.610596,193.808909,158.876765,176.046602')",
+                        offsetRotate: "0deg"
+                    }}
+                >
+                    <ellipse 
+                        rx="29.418776" 
+                        ry="27.096241" 
+                        transform="scale(0.449848,0.449848) translate(0,0)" 
+                        fill="none" 
+                        stroke="currentColor" 
+                        strokeWidth="8"
+                    />
+                </g>
+                <mask 
+                    id="e5EjUZ76vzl6" 
+                    maskType="luminance" 
+                    x="-150%" 
+                    y="-150%" 
+                    height="400%" 
+                    width="400%"
+                >
+                    <rect 
+                        width="48.538102" 
+                        height="50.322591" 
+                        rx="0" 
+                        ry="0" 
+                        transform="matrix(2.234249 0 0 2.789022 172.110742 64.751635)" 
+                        fill="#d2dbed" 
+                        strokeWidth="0"
+                    />
+                </mask>
+            </g>
+        </svg>
+    </button>;
+}
+
+function ProjectsButton({ image, className, onClick, page, id }) {
+  return (
+    <button className={className ? className : "button"} id={id} onClick={onClick}>
+      <svg id="ezmQEsx5krU1" className="icon icon-projects" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300" shapeRendering="geometricPrecision">
+        <g id="ezmQEsx5krU2_to" transform="translate(115.711537,144.207123)">
+          <g id="ezmQEsx5krU2_tr" transform="rotate(0)">
+            <g transform="translate(-115.711536,-144.207123)">
+              <path 
+                d="M83.182916,105.90532v115.666178q149.548726,0,149.548726,0t-.252384-115.666178h-149.296342" 
+                transform="matrix(0 1-1 0 279.449945-13.750162)" 
+                fill="rgba(255,255,255,0.46)" 
+                stroke="currentColor" 
+                strokeWidth="4"
+              />
+              <rect 
+                width="34.700822" 
+                height="31.885102" 
+                rx="0" 
+                ry="0" 
+                transform="translate(72.910969 94.182927)" 
+                fill="rgba(210,219,237,0.29)" 
+                strokeWidth="0"
+              />
+              <ellipse 
+                rx="28.800745" 
+                ry="28.361442" 
+                transform="translate(131.576307 126.068029)" 
+                fill="rgba(249,141,141,0.44)" 
+                strokeWidth="0"
+              />
+              <polygon 
+                points="0,-24.58526 23.381971,-7.597263 14.450853,19.889893 -14.450853,19.889893 -23.381971,-7.597263 0,-24.58526" 
+                transform="matrix(.917907-.396796 0.396796 0.917907 86.356835 176.564601)" 
+                fill="rgba(252,240,155,0.39)" 
+                strokeWidth="0"
+              />
+            </g>
+          </g>
+        </g>
+        <g id="ezmQEsx5krU7_to" transform="translate(165.56147,137.370564)">
+          <g id="ezmQEsx5krU7_tr" transform="rotate(0)">
+            <g transform="translate(-144.970521,-107.464389)">
+              <path 
+                style={{isolation: "isolate"}}
+                d="M83.182916,105.90532v115.666178q149.548726,0,149.548726,0t-.252384-115.666178h-149.296342" 
+                transform="matrix(0 1-1 0 307.868317-22.731685)" 
+                fill="#fff" 
+                stroke="currentColor" 
+                strokeWidth="4"
+              />
+              <path 
+                d="M98.86759,70.19227h63.459087" 
+                transform="translate(1.437414 16.620056)" 
+                fill="none" 
+                stroke="currentColor" 
+                strokeWidth="4"
+              />
+              <path 
+                d="M98.86759,70.19227h87.186606" 
+                transform="translate(1.437414 37.272124)" 
+                fill="none" 
+                stroke="currentColor" 
+                strokeWidth="4"
+              />
+              <path 
+                d="M98.86759,70.19227h87.186606" 
+                transform="translate(1.669015 52.834976)" 
+                fill="none" 
+                stroke="currentColor" 
+                strokeWidth="4"
+              />
+              <path 
+                d="M98.86759,70.19227h63.227486" 
+                transform="translate(1.669015 45.352835)" 
+                fill="none" 
+                stroke="currentColor" 
+                strokeWidth="4"
+              />
+              <path 
+                d="M98.86759,70.19227h63.227486" 
+                transform="translate(1.669015 60.317117)" 
+                fill="none" 
+                stroke="currentColor" 
+                strokeWidth="4"
+              />
+              <path 
+                d="M98.86759,70.19227h31.729543" 
+                transform="translate(1.437414 110.597104)" 
+                fill="none" 
+                stroke="currentColor" 
+                strokeWidth="4"
+              />
+              <path 
+                d="M98.86759,70.19227h67.608683" 
+                transform="translate(1.669015 117.480674)" 
+                fill="none" 
+                stroke="currentColor" 
+                strokeWidth="4"
+              />
+            </g>
+          </g>
+        </g>
+      </svg>
+    </button>
+  );
+}
+function ContactButton({ image, className, onClick, page, id }) {
+  return (
+    <button className={className ? className : "button"} id={id} onClick={onClick}>
+      <svg id="epfso48PGta1" className="icon icon-contact" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300" shapeRendering="geometricPrecision">
+        <g id="epfso48PGta2_tr" transform="translate(151.873475,147.44254) rotate(0)">
+          <g id="epfso48PGta2_ts" transform="scale(1,1)">
+            <path id="epfso48PGta2" d="M65.56857,96.144461v86.383601q0,20.985845,30.936751,20.985845c9.176874,0,10.330003,8.989249,10.330003,23.995475c20.362202,0,74.204649-23.995475,105.687274-23.995475q32.339392,0,32.339392-20.985845c0-20.985845.280297-86.097472.28619-86.383601.304499-14.784459-16.196317-14.784459-16.196317-14.784459q0,0-147.436766-.592895Q65.56857,81.991211,65.56857,96.144461" transform="translate(-151.873472,-147.442536)" fill="none" stroke="currentColor" strokeWidth="4"/>
+          </g>
+        </g>
+      </svg>
+    </button>
+  );
+}
 
 
 root.render(<Page />);
